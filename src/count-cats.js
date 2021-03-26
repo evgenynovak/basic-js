3053d3f5060ceb72d524e0c5a0f5bc12e7661642
evgenynovak <evnovak171@mail.ru>
2021-03-26 23:15:56 +0300

    feat: carbon-dating done count-cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  let number = 0
+  matrix.map(
+    (variant) => {
+      variant.map(
+        (cat)=>{
+          if (cat=='^^') {
+            number = number + 1
+          } 
+        }
+      )
+    }
+  )
+  return number
   // remove line with error and write your code here
 };

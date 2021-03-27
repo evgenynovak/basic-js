1061f498a58c02302eeeac5eb55d5ae375405a4f
evgenynovak <evnovak171@mail.ru>
2021-03-27 21:22:55 +0300

    fix: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,11 +5,15 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   let number = 0
-   if ((!isNaN(sampleActivity)) && (sampleActivity>=0)) {
-    number = Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD)
-    return Math.ceil(number)
-  }
-  
+  // if ((!isNaN(sampleActivity)) && (sampleActivity>=0)) {
+  //  number = Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD)
+  //  return Math.ceil(number)
+  // }
+  if (typeof(sampleActivity) == "string") {
+    let value = Number(sampleActivity)
+      number = Math.log(MODERN_ACTIVITY/value)/(0.693/HALF_LIFE_PERIOD)
+      return Math.ceil(number)
+  } else return false
   return false
 
   // remove line with error and write your code here
